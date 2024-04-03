30c8205f500bbfc25968c37521ea6d56ed0ec09d
kontino <kostis.gouz@gmail.com>
2024-04-03 16:54:42 +0300

    Initial Commit

---
+++ Survey App/Survey_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Survey_AppApp.swift
+//  Survey App
+//
+//  Created by Konstantinos Gouzinis on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Survey_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Survey AppUITests/Survey_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Survey_AppUITestsLaunchTests.swift
+//  Survey AppUITests
+//
+//  Created by Konstantinos Gouzinis on 3/4/24.
+//
+
+import XCTest
+
+final class Survey_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
